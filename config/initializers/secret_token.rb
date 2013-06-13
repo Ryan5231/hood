646a9d6108034a7b08282c3a716f05e1263f5e9f
dbcapprentice <dbcapprentice@dbc.com>
2013-06-13 12:02:21 -0700

    redid rails new with pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hood::Application.config.secret_token = 'e12f48cd6391296c853c140117ae3a87931db2d95bc9bed90a3efa886934d17761a208cbe317ed89b76d28a0392c510bccc6085712b2f6ef31caa9fa989d0b5d'
