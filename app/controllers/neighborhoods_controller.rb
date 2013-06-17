f7e2f71d6dae1e4e7c63f6e7547ece8602e94aad
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-06-17 14:01:16 -0700

    only active listings appear on neighborhood

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,6 +1,6 @@
 class NeighborhoodsController < ApplicationController
   def show
     @hood = Neighborhood.find(params[:id])
-    @listings = Listing.where('neighborhood_id=?', @hood.id)
+    @listings = Listing.where('neighborhood_id=? AND status=?', @hood.id, 'active')
   end
 end
