2a2d6e489f4ba79cf104e9c2b0a8b28267527b86
dbcapprentice <dbcapprentice@dbc.com>
2013-06-18 00:57:04 -0700

    2nd homepage

--- app/assets/javascripts/store_slider.js
+++ app/assets/javascripts/store_slider.js
@@ -1,27 +1,27 @@
-(function() {
+// (function() {
 
-$(document).ready(function() {
-  var slideNumber = 0,
-      $store = $('.stores');
+// $(document).ready(function() {
+//   var slideNumber = 0,
+//       $store = $('.stores');
 
-  function slide() {
-    $store.animate( { marginLeft: '-=100%' }, 1000, popPush);
-  }
+//   function slide() {
+//     $store.animate( { marginLeft: '-=100%' }, 1000, popPush);
+//   }
 
-  function popPush() {
-    var slide = $store.find('li.store-pic:first');
-    var slideClone = slide.clone();
+//   function popPush() {
+//     var slide = $store.find('li.store-pic:first');
+//     var slideClone = slide.clone();
 
-    slideClone.appendTo($store);
-    slide.remove();
-    $store.css('margin-left', 0);
-  }
+//     slideClone.appendTo($store);
+//     slide.remove();
+//     $store.css('margin-left', 0);
+//   }
 
-  setInterval( function() {
-    console.log('hey')
-    slide()
-  }, 4000 )
+//   setInterval( function() {
+//     console.log('hey')
+//     slide()
+//   }, 4000 )
 
-});
+// });
 
-})()
+// })()
