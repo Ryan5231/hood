8c74cc9632027342784c7d3093e6d701691c34a3
Ryan Salomon <ryan5231@gmail.com>
2013-06-18 15:28:27 -0700

    cucumber

---
+++ features/step_definitions/listings_steps.rb
@@ -0,0 +1,34 @@
+def the_listing
+  Listing.last
+end
+
+def login(email, pwd)
+  within '#user_login' do
+    fill_in 'Email', :with => email
+    fill_in 'Password', :with => pwd
+    click_on 'Log In'
+  end
+end
+
+Given(/^I have an existing listing titled "(.*?)"$/)  do |title|
+  FactoryGirl.create(:listing, title: title, :realtor => @user) # express the regexp above with the code you wish you had
+end
+
+Given(/^I am logged in as a user with email "(.*?)" and password "(.*?)"$/) do |email, password|
+  @user = FactoryGirl.create(:user, :email => email, :password => password)
+  @user.neighborhoods << FactoryGirl.create(:neighborhood)
+  @user.save!
+  visit '/'
+  login email, password
+end
+
+When(/^I change the title to "(.*?)"$/) do |title|
+  @expected_title = title
+  visit "/listings/#{the_listing.id}/edit"
+  fill_in 'Title', :with => title
+  click_on 'Edit Listing'
+end
+
+Then(/^I should see the corrected listing$/) do
+  page.should have_content(@expected_title)
+end
