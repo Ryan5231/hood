5a4514bb880801d2846dee109474baf1bff985cd
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-06-19 09:42:40 -0700

    minor styling on listing creation page

--- app/assets/javascripts/choice_forms.js
+++ app/assets/javascripts/choice_forms.js
@@ -1,4 +1,4 @@
-  
+
 $(document).on('ajax:success', '.new_choice', function(event, data){
    var newChoice = $('.choices-listing-template').find(':first-child')[0];
    newChoice = $(newChoice);
@@ -8,21 +8,19 @@ $(document).on('ajax:success', '.new_choice', function(event, data){
 });
 
 
-$(document).on('ajax:success', '.choice_vote', function(event, data){
-  $(this).parent().parent().parent().find('.vote_count').text(data.votes); 
-}); 
+// $(document).on('ajax:success', '.choice_vote', function(event, data){
+//   $(this).parent().parent().parent().find('.vote_count').text(data.votes); 
+// }); 
 
+// $(document).on('ajax:success', '.new_comment', function(event, data){
+//  console.log(data);
+//  var newComment = $($('h3.comment')[0]);
+//  newComment.text(data.content);
+//  $('h3.comment:last-child')after(newComment);
+// });
 
-
-$(document).on('ajax:success', '.new_comment', function(event, data){
- console.log(data);
- var newComment = $($('h3.comment')[0]);
- newComment.text(data.content);
- $('h3.comment:last-child')after(newComment);
-});
-
-$(document).on('click','.new_comment_button', function(event){
-	event.preventDefault;
-	$('.comment_form').removeClass('hidden');
-	$(this).detach();
-});
+// $(document).on('click','.new_comment_button', function(event){
+// 	event.preventDefault;
+// 	$('.comment_form').removeClass('hidden');
+// 	$(this).detach();
+// });
